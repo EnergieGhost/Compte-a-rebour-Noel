fc57b674ba44402c0b5e606987156090b47929ce
Thomas00D <delannoy.thomas.pro@outlook.fr>
2023-12-23 12:57:09 +0100

    fix bug

--- components/Noel/Noel.tsx
+++ components/Noel/Noel.tsx
@@ -1,18 +1,30 @@
 import { Text } from '@mantine/core';
-import classes from './Noel.module.css';
 import { useEffect, useState } from 'react';
+import classes from './Noel.module.css';
 
 export const Noel = () => {
   const [hour, setHour] = useState<string>('00');
   const [minute, setMinute] = useState<string>('00');
   const [second, setSecond] = useState<string>('00');
+  const [noelDate] = useState<Date>(new Date('December 25 , 2023'));
+
+  const formatValue = (value: number): string => {
+    if (value < 10) {
+      return `0${value}`;
+    }
+    return value.toString();
+  };
 
   const calcul = () => {
-    const date = new Date().toTimeString().split(' ');
-    const time = date[0].split(':');
-    setHour(time[0]);
-    setMinute(time[1]);
-    setSecond(time[2]);
+    const today = new Date();
+    const difference = noelDate.getTime() - today.getTime();
+    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+    setHour(formatValue(hours));
+    setMinute(formatValue(minutes));
+    setSecond(formatValue(seconds));
   };
 
   useEffect(() => {
@@ -25,10 +37,13 @@ export const Noel = () => {
 
   return (
     <div className={classes.body}>
-      <h1 className={classes.titre}>Compte à rebours avant Noel</h1>
-      <Text className={classes.label}>
-        {hour}:{minute}:{second}
-      </Text>
+      <div className={classes.container}>
+        <h1 className={classes.h1}>Compte à rebours avant Noel</h1>
+        <Text className={classes.label}>
+          {hour}:{minute}:{second}
+        </Text>
+        <h2 className={classes.h1}>Dev by Twelve57 and Thomas00D</h2>
+      </div>
     </div>
   );
 };
